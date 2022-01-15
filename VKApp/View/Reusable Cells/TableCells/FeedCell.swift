e28b80f4e14aaf6b837a04f405ee6aeadbe7e48b
Alla Shkolnik <a.shkolnik@darknessproduction.com>
2022-01-16 00:10:26 +0300

    скроллвью для просмотра неск фоток в посте

--- VKApp/View/Reusable Cells/TableCells/FeedCell.swift
+++ VKApp/View/Reusable Cells/TableCells/FeedCell.swift
@@ -17,21 +17,25 @@ final class FeedCell: UITableViewCell {
     @IBOutlet weak var feedImage: UIImageView!
     @IBOutlet weak var likeButton: UIButton!
     @IBOutlet weak var replyButton: UIButton!
+    @IBOutlet weak var imgScrollView: UIScrollView!
     
     var feed: Feed?
     
     func configureFeedCell(feed: Feed) {
         self.userName.text = feed.user.userName
-        self.feedCreationDate.text = feed.date.toString(dateFormat: .dateTime)
-        self.userPhotoBackground.backgroundColor = UIColor(cgColor: feed.user.codeColor)
+        feedCreationDate.text = feed.date.toString(dateFormat: .dateTime)
+        userPhotoBackground.backgroundColor = UIColor(cgColor: feed.user.codeColor)
+        
         self.userPhoto.isHidden = feed.user.userPhoto == nil
+        self.imgScrollView.isHidden = feed.image?.img == nil
+        acronym.isHidden = feed.user.userPhoto != nil
+        feedMessage.isHidden = feed.messageText == nil
+        feedImage.isHidden = feed.image == nil
+        
         self.userPhoto.image = feed.user.userPhoto?.img ?? nil
-        self.acronym.isHidden = feed.user.userPhoto != nil
-        self.acronym.text = feed.user.userName.acronym
-        self.feedMessage.isHidden = feed.messageText == nil
-        self.feedMessage.text = feed.messageText
-        self.feedImage.isHidden = feed.image == nil
-        self.feedImage.image = feed.image?.img ?? nil
+        acronym.text = feed.user.userName.acronym
+        feedMessage.text = feed.messageText
+        feedImage.image = feed.image?.img ?? nil
         self.feed = feed
         
     }
@@ -43,9 +47,6 @@ final class FeedCell: UITableViewCell {
         sender.setTitle(String(count), for: .init())
         let imageTitle = sender.isSelected ? "hand.thumbsup.circle.fill" : "hand.thumbsup.circle"
         sender.setImage(UIImage(systemName: imageTitle), for: .init())
-        
     }
-    
-    
     
 }
